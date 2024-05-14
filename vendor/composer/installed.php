bd2fff135e3e884bdde140d81d98549f3d70769a
Alexanders157 <Alexander.157@yandex.ru>
2024-05-14 22:21:27 +0300

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'token-generator/random-token-generator-alexander157',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0e3e8fff48af7bfd6ea87597648ef66671570f92',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'token-generator/random-token-generator-alexander157' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0e3e8fff48af7bfd6ea87597648ef66671570f92',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
